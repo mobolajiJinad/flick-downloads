aa5b8fbcd1c053143018a89b47f4a327f7596959
omobolaji <abdjinadquadri@gmail.com>
2023-03-26 07:17:01 +0100

    Improved recommendation engine

--- controllers/user/recommend.js
+++ controllers/user/recommend.js
@@ -1,30 +1,56 @@
-const _ = require("lodash");
-
 const Preference = require("../../models/Preference");
 const ViewingHistory = require("../../models/ViewingHistory");
-const getSimilarMovies = require("../../utils/recommendMovies");
-const { StatusCodes } = require("http-status-codes");
 
-const recommendMovies = async (req, res) => {
+const { StatusCodes } = require("http-status-codes");
+const getRecommendations = require("../../utils/recommendMovies");
+
+const recommendMovies = async (req, res, next) => {
   const { userID } = req.user;
 
-  const userPreferences = await Preference.findOne({ preferredBy: userID });
-  const viewingHistory = await ViewingHistory.find({ viewedBy: userID });
+  try {
+    const userPreferences = await Preference.findOne({ preferredBy: userID });
+    const viewingHistory = await ViewingHistory.find({ viewedBy: userID })
+      .sort({ rating: -1 })
+      .where("rating")
+      .gte(3);
 
-  const likedGenres = _.map(userPreferences.genres, "_id");
-  const likedActors = userPreferences.actors;
-  const likedMovies = userPreferences.movies.concat(
-    _.map(viewingHistory, "title")
-  );
+    const actorsByGenre = groupByGenre(userPreferences.actors);
+    const moviesByGenre = groupByGenre(userPreferences.movies);
 
-  const similarMovies = await getSimilarMovies(
-    likedGenres,
-    likedActors,
-    likedMovies
-  );
+    const genres = [...new Set(viewingHistory.map((item) => item.genre))];
+    const similarMoviesPromises = genres.map(async (genreId) => {
+      const genreActors = actorsByGenre[genreId] || [];
+      const genreMovies = moviesByGenre[genreId] || [];
+      const genreViewingHistory = viewingHistory.filter(
+        (item) => item.genre === genreId
+      );
 
-  const result = _.map(similarMovies, "title").slice(0, 10);
-  res.status(StatusCodes.OK).json(result);
+      const similarMovies = await getRecommendations(
+        genreId,
+        genreActors,
+        genreMovies
+      );
+
+      return similarMovies.slice(0, 10);
+    });
+
+    const similarMovies = await Promise.all(similarMoviesPromises);
+    const flattenedMovies = similarMovies.flat();
+
+    res.status(StatusCodes.OK).json(flattenedMovies);
+  } catch (error) {
+    next(error);
+  }
+};
+
+const groupByGenre = (items) => {
+  return items.reduce((acc, item) => {
+    if (!acc[item.genreId]) {
+      acc[item.genreId] = [];
+    }
+    acc[item.genreId].push(item);
+    return acc;
+  }, {});
 };
 
 module.exports = recommendMovies;
--- utils/recommendMovies.js
+++ utils/recommendMovies.js
@@ -1,42 +1,43 @@
 const _ = require("lodash");
 
-const { StatusCodes } = require("http-status-codes");
 const { CustomError } = require("../middleware/error-handler");
 
-const getRecommendations = async (likedGenres, likedActors, likedMovies) => {
+const getRecommendations = async (genreId, genreActors, genreMovies) => {
   const queryParams = new URLSearchParams({
     api_key: process.env.API_KEY,
     language: "en-US",
     page: 1,
-    with_genres: likedGenres.join("|"),
-    with_cast: likedActors.join("|"),
-    query: likedMovies.join(" "),
-    sort_by: "vote_average.desc",
+    with_genres: genreId,
+    with_cast: genreActors.map((actor) => actor.tmdbId).join(","),
+    query: genreMovies.map((movie) => movie.name).join(" "),
+    sort_by: "popularity.desc",
   });
-  console.log(queryParams);
+
   const apiUrl = `https://api.themoviedb.org/3/discover/movie?${queryParams}`;
 
-  const response = await fetch(apiUrl);
-  const data = await response.json();
+  try {
+    const response = await fetch(apiUrl);
 
-  if (response.status !== 200) {
-    throw new CustomError(
-      "Error fetching recommendations",
-      StatusCodes.INTERNAL_SERVER_ERROR
-    );
+    if (!response.ok) {
+      throw new Error("Error fetching recommendations");
+    }
+
+    const results = await response.json();
+
+    const recommendedMovies = results.results.map((result) => {
+      return {
+        id: result.id,
+        title: result.title,
+        genres: result.genre_ids,
+        overview: result.overview,
+        rating: result.vote_average,
+      };
+    });
+
+    return recommendedMovies;
+  } catch (error) {
+    throw new CustomError(error.message, 500);
   }
-
-  const similarMovies = _.map(data.results, (movie) => {
-    return {
-      id: movie.id,
-      title: movie.title,
-      genres: movie.genre_ids,
-      overview: movie.overview,
-      rating: movie.vote_average,
-    };
-  });
-
-  return similarMovies;
 };
 
 module.exports = getRecommendations;
